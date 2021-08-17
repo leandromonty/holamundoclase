894201117604ab76833c6550bafc05fd28cbb981
Leandro Montivero <leandromonty@gmail.com>
2021-08-17 18:44:42 -0300

    variables y ejerecicio

---
+++ js/holamundo.js
@@ -0,0 +1,34 @@
+//comentario simple
+/*comentario de lineas multiples */
+
+/* mostrar mensaje en consola */
+
+console.log('Hola comision 17')
+
+//mostrar un mensaje en la pag html
+document.write("Este es mi primer mensaje")
+
+//ventana emergente
+//alert('esto es una alerta')
+
+//crear o declarar vaiables var - let - const
+
+let palabraBuscada = 'hola'
+
+document.write('<br>variable palabraBuscada: ' + palabraBuscada )
+
+//modificar el valor de una variable
+
+palabraBuscada = 'pagina de blog'
+
+document.write('<br>variable palabraBuscada: ' + palabraBuscada )
+
+const comision = 17
+
+document.write('<br>constante comision: ' + comision )
+
+let luz = true;
+
+let suma = 20
+console.log(++suma)
+console.log(suma +1)

484d70862c9679451da769ae02a95f32a1496b26
Leandro Montivero <leandromonty@gmail.com>
2021-08-19 16:58:33 -0300

    solucion ejercicio 5

---
+++ js/ejercicio1-5.js
@@ -0,0 +1,6 @@
+//parseInt('numero') = se transforma a numero entero
+let numero1 = parseInt (prompt('ingrese primer numero'))
+let numero2 = 50;
+
+console.log(numero1)
+console.log(numero1 + numero2)

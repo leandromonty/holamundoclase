2cb966cb3fe4837e52a6c03472fa8cafbb0315c2
Leandro Montivero <leandromonty@gmail.com>
2021-08-19 17:35:54 -0300

    se creo condicionales

--- js/ejercicio1-5.js
+++ js/ejercicio1-5.js
@@ -1,6 +1,7 @@
 //parseInt('numero') = se transforma a numero entero
-let numero1 = parseInt (prompt('ingrese primer numero'))
-let numero2 = 50;
+let numero1 = parseInt (prompt('ingrese un primer numero'))
+let numero2 = parseInt (prompt('ingrese un segundo numero'))
 
-console.log(numero1)
-console.log(numero1 + numero2)
+/*console.log(numero1)
+console.log(numero1 + numero2)*/
+document.write(numero1+numero2)
---
+++ js/estructuraCondicional.js
@@ -0,0 +1,18 @@
+/* 
+if(condicion logica){
+    Ccodigo a ejecutar si se cumple la condicion
+}else{
+    codigo a ejecutar si no se cumple la condicion
+}
+*/
+
+let numero1 = parseInt(prompt("ingrese un primer numero"));
+let numero2 = parseInt(prompt("ingrese un segundo numero"));
+
+if (numero1 > numero2) {
+  document.write("La variable numero1 es mayor" + numero1);
+} else if (numero2 > numero1) {
+  document.write("La variable numero2 es mayor" + numero2);
+} else {
+  document.write("Los numeros son iguales");
+}

8cfd7ff55e3070c0733be746f7a272a4a3a983ca
Leandro Montivero <leandromonty@gmail.com>
2021-09-02 18:42:44 -0300

    se cargan funciones

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -5,6 +5,16 @@ function aumentarPrecio(){
     document.write('Valor total del servicio disney plus $' + (disney + inflacion))
 }
 
+// funcion con parametros
+function actualizarPrecio(monto, producto, precio){
+    let calculo= precio + monto
+    document.write('<br> el producto; '+producto+ ' precio total $'+ calculo)
+}
 
 // invocar funcion
 aumentarPrecio();
+
+let coeficienteIflacion = parseInt(prompt('ingrese el monto total del mes'))
+
+
+actualizarPrecio(coeficienteIflacion, 'Teclado Hyperx',9000)

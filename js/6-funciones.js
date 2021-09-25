2a08fa9c282763b74c99abb72dc421d7e770f901
Leandro Montivero <leandromonty@gmail.com>
2021-09-25 20:54:52 -0300

    ?objetos?

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -8,15 +8,37 @@ function aumentarPrecio(){
 // funcion con parametros
 function actualizarPrecio(monto, producto, precio){
     let calculo= precio + monto
-    document.write('<br> el producto; '+producto+ ' precio total $'+ calculo)
+    document.write('<br> el producto; '+producto+ ' precio total $'+ calculo);
+}
+
+//funciones que devuelven un valor
+/*function convertirDolaresPesos(precioDolar){
+let total = precioDolar * 184;
+return total;
+//jamas escribimos en esta zona la linea return es la ultima linea
+}*/
+
+//funciones en formas de flechas
+let convertirDolaresPesos = (precioDolar, dolarBlue) =>{
+    let total = precioDolar * 184;
+    return total;
 }
 
 // invocar funcion
 aumentarPrecio();
 
-let coeficienteIflacion = parseInt(prompt('ingrese el monto total del mes'))
+let coeficienteIflacion = parseInt(prompt('ingrese el monto total del mes'));
 
-actualizarPrecio(coeficienteIflacion, 'Teclado Hyperx',9000)
-actualizarPrecio(coeficienteIflacion, 'Teclado logitec',9000)
-actualizarPrecio('mouse logitec',9000, 100)
+actualizarPrecio(coeficienteIflacion, 'Teclado Hyperx',9000);
+actualizarPrecio(coeficienteIflacion, 'Teclado logitec',9000);
+actualizarPrecio('mouse logitec',9000, 100);
 
+
+let dolarBlue = parseInt(prompt ('ingrese calor del dolar blue a la fecha'));
+let iphone = convertirDolaresPesos(1200)
+
+document.write('<br>Producto Iphone - precio$' +iphone+'(producto nuevo)');
+document.write('<br>Producto Ps5 - precio $' + convertirDolaresPesos(500)+'(ultima version)');
+
+//document.write(`<br>Producto Iphone - precio$ ${iphone} (producto nuevo)`);
+ 
---
+++ js/7-objetos.js
@@ -0,0 +1,46 @@
+ let mostrarObjeto = (serieParametro) =>{
+     //obtener todas las claves de un objeto si fuera un arreglo
+     let claves = Object.keys(serieParametro)
+     console.log(claves)
+     for(let i = 0; i> claves.length; i ++){
+         document.write(`<br>${claves[i]}: ${serieParametro[claves[i]]}`)
+     }
+ }
+
+ /*let nombre  = 'the office';
+ let temporada = 9;
+ let genero = 'sitcom';
+ let capitulos = 27;*/
+
+ // crear objero con notacion literal
+
+ let serie ={
+     nombre : 'The Office',
+     temporadas: 9,
+     genero: 'sitcom',
+     capitulos: 27
+ }
+
+ //mostrar una propiedad de un objeto
+ document.write(`serie: ${serie.nombre}`)
+ document.write(`<br>Capitulos: ${serie.capitulos}`)
+ //mostrar una propiedad de un objetodo metodo 2
+ document.write(`<br>temporadas: ${serie['temporadas']}`)
+ console.log(serie)
+
+ //modificar una propiedad de un objeto  
+ serie.temporadas = 10;
+
+ document.write(`<br>temporadas: ${serie['temporadas']}`)
+
+//agregar una nueva propiedad al objeto
+serie.premios = true
+document.write(`<br>premios: ${serie.premios}`)
+
+//borrar un objeto 
+delete serie.genero
+console.log(serie)
+
+//llamamos a la funcion
+mostrarObjeto(serie)
+

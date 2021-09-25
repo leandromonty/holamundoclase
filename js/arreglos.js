f2c033d0131231045c71f71cb453c54f690d535a
Leandro Montivero <leandromonty@gmail.com>
2021-09-25 16:03:27 -0300

    arreglos

---
+++ js/arreglos.js
@@ -0,0 +1,44 @@
+let anime= 'Dragon Ball z'
+
+
+//crear o inicializar un arreglo
+let series = ['Dragon Ball ','Naruto', 'Evangelion 3.0+1', anime, 15, true]
+// crear un arreglo vacio
+let temporadas= [];
+
+document.write('serie individual:' + series[3] );
+
+
+//agregar un elemento al final del arreglo
+series.push('Loky');
+series.push('Breaking Bad');
+
+console.log(series[6]);
+
+// para saber la cantidad de elemento  que tiene el arrelgo
+console.log(series.length);
+console.log(temporadas.length);
+
+series.push(prompt('ingrese una serie '));
+console.log(series.length);
+
+//mostrar todos los elementos de un arreglo
+for(let posicion = 0; posicion < series.length; posicion++){
+    document.write('<br>Series:'+series[posicion])
+}
+
+//modificar un elemento del arreglo
+series[4] = 'The Office';
+
+document.write('<br>');
+for(let posicion = 0; posicion < series.length; posicion++){
+    document.write('<br>Series:'+series[posicion])
+}
+
+//agregar elemento nuevo en una posicion particular
+series.splice(0,0,'One Piece')
+
+document.write('<br>');
+for(let posicion = 0; posicion < series.length; posicion++){
+    document.write('<br>Series:'+series[posicion])
+}
